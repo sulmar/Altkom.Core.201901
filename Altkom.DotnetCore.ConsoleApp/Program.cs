246368cb6470857e06f1aa6fd2abe41a21568e00
Marcin Sulecki <marcin.sulecki@gmail.com>
2019-01-21 16:06:41 +0100

    day 1

---
+++ Altkom.DotnetCore.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Altkom.DotnetCore.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
